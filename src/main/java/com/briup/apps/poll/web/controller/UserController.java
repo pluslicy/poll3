0b4a87b5afdbc3469c10cbcd9aebf435f5dc57d3
李春雨 <@192.168.15.129>
2018-06-25 17:54:44 +0800

    初始化用户模块功能

---
+++ src/main/java/com/briup/apps/poll/web/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.briup.apps.poll.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.Api;
+
+@Api(description="用户相关的接口")
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+}
